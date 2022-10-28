4440d514bd6a9856ee65a884a3dc5c49e219af35
Jxxunnn <roof2586@naver.com>
2022-10-29 00:30:39 +0900

    Feat: 전체 선택 기능

--- functional-checkbox/main.js
+++ functional-checkbox/main.js
@@ -1,8 +1,10 @@
 const $inbox = document.querySelector(".inbox");
 const $inputs = $inbox.querySelectorAll(".item > input");
 let isShiftHolded = false;
+let firstCheck = null;
+let lastCheck = null;
 
-const inspectIsShiftHolded = (e) => {
+const detectIsShiftHolded = (e) => {
   if (e.key === "Shift") {
     if (e.type === "keydown") {
       isShiftHolded = true;
@@ -12,11 +14,24 @@ const inspectIsShiftHolded = (e) => {
     }
   }
 };
+const checkInbetween = (e) => {
+  if (isShiftHolded && e.target.checked === true && !lastCheck) {
+    firstCheck = e.target.dataset.indexNumber;
+    console.log(firstCheck);
+  }
+  if (isShiftHolded && e.target.checked === true && firstCheck) {
+    lastCheck = e.target.dataset.indexNumber;
+    const [max, min] = [
+      Math.max(firstCheck, lastCheck),
+      Math.min(firstCheck, lastCheck),
+    ];
+    for (const input of $inputs) {
+      const idx = input.dataset.indexNumber;
+      if (idx > min && idx < max) input.checked = true;
+    }
+  }
+};
 
-console.log($inputs);
-
-const checkInbetween = () => {};
-
-window.addEventListener("keydown", inspectIsShiftHolded);
-$inbox.addEventListener("click", checkInbetween);
-window.addEventListener("keyup", inspectIsShiftHolded);
+window.addEventListener("keydown", detectIsShiftHolded);
+$inbox.addEventListener("change", checkInbetween);
+window.addEventListener("keyup", detectIsShiftHolded);
